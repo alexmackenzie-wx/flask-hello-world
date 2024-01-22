b0fa52f76d568bb2676062936e0ace42e653e9d4
Alex Mackenzie at Tapestry VC <44316926+alexmackenzie-wx@users.noreply.github.com>
2024-01-22 11:39:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def homepage():
+    return "Armin, would love to chat sometime: <a href='mailto:alex@tapestry.vc'>alex@tapestry.vc</a>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
