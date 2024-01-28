7bf714dbf69121bd0fc8a9c85a42fddc4576cfac
Alex Mackenzie at Tapestry VC <44316926+alexmackenzie-wx@users.noreply.github.com>
2024-01-28 20:19:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Armin, would love to chat sometime: <a href='mailto:alex@tapestry.vc'>alex@tapestry.vc</a>"
+    return "check out my actual blog at: mack.work"
 
 @app.route('/example', methods=['GET'])
 def example_route():
     # Your custom function logic goes here
     # This function will be invoked when a GET request is made to "/example"
-    return "Hello Why Now reader, consider subscribing?"
+    return "What’s up, h@ckɇr."
 
 if __name__ == '__main__':
     app.run(debug=True)
